0e311b10fbf3998aa9a1d1039a08d5d8e93a6b45
Billy <tnehd1998@gmail.com>
2021-12-20 14:05:13 +0900

    메인 페이지에 팀 로고 출력 및 디자인

--- src/components/Introduction/Introduction.jsx
+++ src/components/Introduction/Introduction.jsx
@@ -1,5 +1,35 @@
 import React, { useEffect, useState } from "react";
 import { getTeamData } from "../../apis";
+import styled from "styled-components";
+
+const TeamContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+`;
+
+const TeamMenu = styled.div`
+  width: 200px;
+  height: 200px;
+  border-radius: 15%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 3px solid ${(props) => "#" + props.iconBgColor};
+  &:hover {
+    transition: background-color 0.5s ease-in;
+    background-color: ${(props) => "#" + props.iconBgColor};
+  }
+`;
+
+const TeamImage = styled.img`
+  width: 100px;
+  height: 100px;
+
+  &:hover {
+    transition: transform 0.5s ease-in;
+    transform: scale(1.2);
+  }
+`;
 
 const Introduction = () => {
   const [info, setInfo] = useState();
@@ -17,7 +47,25 @@ const Introduction = () => {
   return (
     <div>
       <button onClick={() => console.log(info)}>Check</button>
-      {!loading ? <div>Loading</div> : <div>Data Loaded</div>}
+      {!loading ? (
+        <div>Loading</div>
+      ) : (
+        <TeamContainer>
+          {info.map((item) =>
+            item.WikipediaLogoUrl ? (
+              <TeamMenu key={item.Key} iconBgColor={`${item.PrimaryColor}`}>
+                <TeamImage
+                  key={item.TeamID}
+                  src={`${item.WikipediaLogoUrl}`}
+                  alt={item.City}
+                />
+              </TeamMenu>
+            ) : (
+              ""
+            )
+          )}
+        </TeamContainer>
+      )}
     </div>
   );
 };
