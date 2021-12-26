22707daf500d5bfd69a15a8bd247b45aa6dfadcb
Billy <tnehd1998@gmail.com>
2021-12-26 14:02:18 +0900

    TeamTitle 디자인 수정

--- src/components/TeamTitle/TeamTitle.jsx
+++ src/components/TeamTitle/TeamTitle.jsx
@@ -1,14 +1,44 @@
 import React from "react";
+import styled from "styled-components";
+
+const TeamTitleContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 90vw;
+  margin-bottom: 2vh;
+`;
+
+const TeamLogoImage = styled.img`
+  width: 15vw;
+  height: 15vw;
+`;
+
+const TeamTitleInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-left: 10vw;
+`;
+
+const TeamTitleCity = styled.p`
+  font-size: 2em;
+`;
+
+const TeamTitleName = styled.p`
+  font-size: 2.5em;
+`;
 
 const TeamTitle = ({ team }) => {
   return (
-    <div>
-      <img src={`${team.WikipediaLogoUrl}`} />
-      <div>
-        <h1>{team.City}</h1>
-        <h1>{team.Name}</h1>
-      </div>
-    </div>
+    <TeamTitleContainer>
+      <TeamLogoImage src={`${team.WikipediaLogoUrl}`} />
+      <TeamTitleInfo>
+        <TeamTitleCity>{team.City}</TeamTitleCity>
+        <TeamTitleName>{team.Name}</TeamTitleName>
+      </TeamTitleInfo>
+    </TeamTitleContainer>
   );
 };
 
