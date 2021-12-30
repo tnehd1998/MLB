97e3c234930d3b0028f0945ffc1ec9a519a9090f
Billy <tnehd1998@gmail.com>
2021-12-30 09:49:10 +0900

    PlayerDetail에 Youtube 동영상 추가

--- src/apis.js
+++ src/apis.js
@@ -1,10 +1,13 @@
 import axios from "axios";
 
-const API_KEY = process.env.REACT_APP_SPORT_DATA_API_KEY;
+const SPORT_DATA_API_KEY = process.env.REACT_APP_SPORT_DATA_API_KEY;
+const YOUTUBE_API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 
 export const getTeamData = async () => {
   const info = await axios
-    .get(`https://api.sportsdata.io/v3/mlb/scores/json/AllTeams?key=${API_KEY}`)
+    .get(
+      `https://api.sportsdata.io/v3/mlb/scores/json/AllTeams?key=${SPORT_DATA_API_KEY}`
+    )
     .then((response) => response.data);
   return info;
 };
@@ -12,7 +15,7 @@ export const getTeamData = async () => {
 export const getCertainTeamData = async (teamName) => {
   const info = await axios
     .get(
-      `https://api.sportsdata.io/v3/mlb/scores/json/Players/${teamName}?key=${API_KEY}`
+      `https://api.sportsdata.io/v3/mlb/scores/json/Players/${teamName}?key=${SPORT_DATA_API_KEY}`
     )
     .then((response) => response.data);
   return info;
@@ -21,8 +24,17 @@ export const getCertainTeamData = async (teamName) => {
 export const getAllStarPlayerData = async () => {
   const info = await axios
     .get(
-      `https://api.sportsdata.io/v3/mlb/projections/json/PlayerSeasonProjectionStats/2021?key=${API_KEY}`
+      `https://api.sportsdata.io/v3/mlb/projections/json/PlayerSeasonProjectionStats/2021?key=${SPORT_DATA_API_KEY}`
     )
     .then((response) => response.data.slice(0, 200));
+  return info;
+};
+
+export const getPlayerVideo = async (playerName, videoCount) => {
+  const info = await axios
+    .get(
+      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=${videoCount}&q=${playerName}&type=video&key=${YOUTUBE_API_KEY}`
+    )
+    .then((response) => response.data);
   return info;
 };
--- src/components/PlayerDetail/PlayerDetail.jsx
+++ src/components/PlayerDetail/PlayerDetail.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { useEffect, useState } from "react/cjs/react.development";
 import { useRecoilValue } from "recoil";
 import styled from "styled-components";
+import { getPlayerVideo } from "../../apis";
 import { currentX, currentY } from "../../atoms";
 
 const ChosenPlayer = styled.div`
@@ -19,8 +21,23 @@ const ChosenPlayer = styled.div`
 `;
 
 const PlayerDetail = ({ showingPlayer, setSelectPlayer }) => {
+  const [videos, setVideos] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
   const scrollX = useRecoilValue(currentX);
   const scrollY = useRecoilValue(currentY);
+
+  useEffect(() => {
+    async function getVideos() {
+      const data = await getPlayerVideo(
+        `${showingPlayer.FirstName} ${showingPlayer.LastName}`,
+        4
+      );
+      setVideos(data);
+      setIsLoading((isLoading) => !isLoading);
+    }
+    getVideos();
+  }, [showingPlayer.FirstName, showingPlayer.LastName]);
 
   return (
     <ChosenPlayer top={scrollY} left={scrollX}>
@@ -29,6 +46,21 @@ const PlayerDetail = ({ showingPlayer, setSelectPlayer }) => {
       </h1>
       <img src={showingPlayer.PhotoUrl} alt="" />
       <h1>Position {showingPlayer.Position}</h1>
+      {isLoading ? (
+        <h1>Loading...</h1>
+      ) : (
+        videos.items.map((video, index) => (
+          <iframe
+            key={index}
+            type="text/html"
+            title={"Video"}
+            src={`https://www.youtube.com/embed/${video.id.videoId}?autoplay=1`}
+            frameBorder="0"
+            allow="autoplay"
+            allowFullScreen
+          ></iframe>
+        ))
+      )}
       <button onClick={() => setSelectPlayer((selectPlayer) => !selectPlayer)}>
         X
       </button>
