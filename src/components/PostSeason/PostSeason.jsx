0a7596c54409ab28b1a28767bdf6abf720bceaa9
이수동 <tnehd1998@gmail.com>
2022-01-13 11:04:47 +0900

    style: Team Standing 디자인 개선

--- src/components/PostSeason/PostSeason.jsx
+++ src/components/PostSeason/PostSeason.jsx
@@ -18,20 +18,80 @@ const Title = styled.p`
 `;
 
 const ScheduleImage = styled.img`
-  width: 70vw;
+  width: 60vw;
   border-radius: 20px;
   border: 2px solid black;
 `;
 
-const StandingWrapper = styled.div``;
+const StandingWrapper = styled.div`
+  width: 60vw;
+  border-radius: 20px;
+  height: 20em;
+  border: 2px solid black;
+`;
 
-const RegionCategories = styled.ul``;
+const RegionCategories = styled.ul`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-bottom: 2px solid black;
+`;
+
+const RegionCategory = styled.li`
+  padding: 1em;
+  cursor: pointer;
+  &:hover {
+    transition: all 0.5s ease-in;
+    background-color: black;
+    color: white;
+  }
+`;
+
+const StandingTeamInfo = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 18px;
+  text-align: center;
+  padding-top: 0.5em;
+`;
+
+const StandingTeams = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StandingTeam = styled(Link)`
+  color: black;
+  text-decoration: none;
+  display: flex;
+  padding: 1em 0;
+  width: 100%;
+  text-align: center;
+  &:hover {
+    font-size: 18px;
+    transition: all 0.5s ease-in;
+    background-color: black;
+    color: white;
+    border-radius: 20px;
+  }
+`;
+
+const StandingTeamName = styled.p`
+  width: 40%;
+`;
+
+const StandingTeamRecord = styled.p`
+  width: 20%;
+`;
 
 const PostSeason = () => {
   const [standing, setStanding] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
-  const [currentRegion, setCurrentRegion] = useState("");
+  const [currentRegion, setCurrentRegion] = useState("AL East");
 
   useEffect(() => {
     async function fetchData() {
@@ -54,28 +114,46 @@ const PostSeason = () => {
       ) : (
         <StandingWrapper>
           <RegionCategories>
-            <li onClick={() => setCurrentRegion("AL East")}>AL East</li>
-            <li onClick={() => setCurrentRegion("AL Central")}>AL Central</li>
-            <li onClick={() => setCurrentRegion("AL West")}>AL West</li>
-            <li onClick={() => setCurrentRegion("NL East")}>NL East</li>
-            <li onClick={() => setCurrentRegion("NL Central")}>NL Central</li>
-            <li onClick={() => setCurrentRegion("NL West")}>NL West</li>
+            <RegionCategory onClick={() => setCurrentRegion("AL East")}>
+              AL East
+            </RegionCategory>
+            <RegionCategory onClick={() => setCurrentRegion("AL Central")}>
+              AL Central
+            </RegionCategory>
+            <RegionCategory onClick={() => setCurrentRegion("AL West")}>
+              AL West
+            </RegionCategory>
+            <RegionCategory onClick={() => setCurrentRegion("NL East")}>
+              NL East
+            </RegionCategory>
+            <RegionCategory onClick={() => setCurrentRegion("NL Central")}>
+              NL Central
+            </RegionCategory>
+            <RegionCategory onClick={() => setCurrentRegion("NL West")}>
+              NL West
+            </RegionCategory>
           </RegionCategories>
-          <p>Team | Wins | Losses</p>
-          <div>
-            {standing.map((team) => {
-              if (
-                team.League === currentRegion.split(" ")[0] &&
-                team.Division === currentRegion.split(" ")[1]
-              ) {
-                return (
-                  <Link key={team.TeamID} to={`/${team.Key}`}>
-                    {team.City} {team.Name} {team.Wins} {team.Losses}
-                  </Link>
-                );
-              }
-            })}
-          </div>
+          <StandingTeamInfo>
+            <StandingTeamName>Team</StandingTeamName>
+            <StandingTeamRecord>Total</StandingTeamRecord>
+            <StandingTeamRecord>Wins</StandingTeamRecord>
+            <StandingTeamRecord>Losses</StandingTeamRecord>
+          </StandingTeamInfo>
+          <StandingTeams>
+            {standing.map((team) =>
+              team.League === currentRegion.split(" ")[0] &&
+              team.Division === currentRegion.split(" ")[1] ? (
+                <StandingTeam key={team.TeamID} to={`/${team.Key}`}>
+                  <StandingTeamName>
+                    {team.City} {team.Name}
+                  </StandingTeamName>
+                  <StandingTeamRecord>162</StandingTeamRecord>
+                  <StandingTeamRecord>{team.Wins}</StandingTeamRecord>
+                  <StandingTeamRecord>{team.Losses}</StandingTeamRecord>
+                </StandingTeam>
+              ) : null
+            )}
+          </StandingTeams>
         </StandingWrapper>
       )}
     </PostSeasonWrapper>
