3b549de754ed2681aa3576a4af30f4009aac33db
이수동 <tnehd1998@gmail.com>
2022-01-28 18:17:56 +0900

    fix: 외부 api 대신 간단하게 구축한 서버로 변경

--- src/apis/apis.js
+++ src/apis/apis.js
@@ -1,29 +1,29 @@
 import axios from "axios";
 
+const baseURL = process.env.REACT_APP_BASE_URL;
+
 export const getTeamData = async () => {
-  const info = await axios
-    .get("http://localhost:8080/")
-    .then((response) => response.data);
+  const info = await axios.get(`${baseURL}`).then((response) => response.data);
   return info;
 };
 
 export const getCertainTeamData = async (teamName) => {
   const info = await axios
-    .get(`http://localhost:8080/${teamName}`)
+    .get(`${baseURL}/${teamName}`)
     .then((response) => response.data);
   return info;
 };
 
 export const getAllStarPlayerData = async () => {
   const info = await axios
-    .get("http://localhost:8080/allstar")
+    .get(`${baseURL}/allstar`)
     .then((response) => response.data.slice(0, 200));
   return info;
 };
 
 export const getTeamStandingData = async () => {
   const info = await axios
-    .get("http://localhost:8080/standing")
+    .get(`${baseURL}/standing`)
     .then((response) => response.data);
   return info;
 };
