52f829871dc573310bb2fc8169c8546454b04112
이수동 <tnehd1998@gmail.com>
2022-01-30 10:01:57 +0900

    feat: 다크모드/라이트모드 type 추가

--- src/styles/theme.js
+++
@@ -1,9 +0,0 @@
-export const lightTheme = {
-  bgColor: "#f5f6fa",
-  textColor: "#252525",
-};
-
-export const darkTheme = {
-  bgColor: "#252525",
-  textColor: "#f5f6fa",
-};
---
+++ src/styles/theme.ts
@@ -0,0 +1,11 @@
+import { DefaultTheme } from "styled-components";
+
+export const lightTheme: DefaultTheme = {
+  bgColor: "#f5f6fa",
+  textColor: "#252525",
+};
+
+export const darkTheme: DefaultTheme = {
+  bgColor: "#252525",
+  textColor: "#f5f6fa",
+};
