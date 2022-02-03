1675d482db68cc2a58fd4a7e4db3a12dba1bd22d
이수동 <tnehd1998@gmail.com>
2022-02-03 21:27:46 +0900

    fix: 테스트 코드를 위해 standing 정보를 컴포넌트에서 직접 useQuery로 받아오는 것으로 변경

--- src/api/standing.ts
+++ src/api/standing.ts
@@ -1,10 +1,9 @@
 import axios from "axios";
-import { useQuery } from "react-query";
 import { IStanding } from "../types/standing.type";
 
 const baseURL = process.env.REACT_APP_BASE_URL;
 
-const getStanding = async (): Promise<IStanding[] | null> => {
+export const getStanding = async (): Promise<IStanding[] | null> => {
   try {
     const info = await axios
       .get(`${baseURL}/standing`)
@@ -13,10 +12,4 @@ const getStanding = async (): Promise<IStanding[] | null> => {
   } catch (e) {
     return null;
   }
-};
-
-export const useGetStanding = () => {
-  return useQuery("standing", getStanding, {
-    suspense: true,
-  });
 };
