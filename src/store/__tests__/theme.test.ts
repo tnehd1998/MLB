8ab5579f6a1f144b9b27d4784033cec9c0cbcf1b
이수동 <tnehd1998@gmail.com>
2022-02-04 16:26:03 +0900

    test: theme atom 테스트 파일 작성

---
+++ src/store/__tests__/theme.test.ts
@@ -0,0 +1,18 @@
+import { Snapshot, snapshot_UNSTABLE } from "recoil";
+import { themeState } from "../theme";
+
+describe("Theme", () => {
+  let initialSnapshot: Snapshot;
+  beforeEach(() => {
+    initialSnapshot = snapshot_UNSTABLE();
+  });
+
+  it("checks initial themeState", () => {
+    expect(initialSnapshot.getLoadable(themeState).valueOrThrow()).toBe(true);
+  });
+
+  it("checks initial themeState to false", () => {
+    const testSnapshot = snapshot_UNSTABLE(({ set }) => set(themeState, false));
+    expect(testSnapshot.getLoadable(themeState).valueOrThrow()).toBe(false);
+  });
+});
