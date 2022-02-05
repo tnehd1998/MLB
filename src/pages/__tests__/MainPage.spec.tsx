c87528bbfe46b4ae1737811a4c4fd5beef429960
이수동 <tnehd1998@gmail.com>
2022-02-05 15:06:07 +0900

    test: MainPage title, content 테스트 파일 작성

--- src/pages/__tests__/MainPage.spec.tsx
+++ src/pages/__tests__/MainPage.spec.tsx
@@ -1,15 +1,31 @@
-import { render } from "@testing-library/react";
+import { render, RenderResult, waitFor } from "@testing-library/react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import MainPage from "../MainPage";
 
-test("<MainPage />", () => {
-  const queryClient = new QueryClient();
-  const { getByText } = render(
-    <QueryClientProvider client={queryClient}>
-      <MainPage />
-    </QueryClientProvider>
-  );
-  const mainText = getByText("Loading...");
+describe("<MainPage />", () => {
+  let renderResult: RenderResult;
+  let queryClient;
+  beforeEach(async () => {
+    await waitFor(() => {
+      queryClient = new QueryClient();
+      renderResult = render(
+        <QueryClientProvider client={queryClient}>
+          <MainPage />
+        </QueryClientProvider>
+      );
+    });
+  });
 
-  expect(mainText).toBeInTheDocument();
+  it("should render OK", async () => {
+    await waitFor(() => {
+      expect(document.title).toBe("MLB | TEAMS");
+    });
+  });
+
+  it("waits page to get teams", () => {
+    const { getByText } = renderResult;
+    const mainText = getByText("Loading...");
+
+    expect(mainText).toBeInTheDocument();
+  });
 });
