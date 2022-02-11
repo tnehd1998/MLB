73ad507542bac0c2901344a314f9f1c3cca0a3b5
이수동 <tnehd1998@gmail.com>
2022-02-11 14:30:33 +0900

    style: Payroll 디자인 개선

--- src/components/organisms/Payroll/Payroll.tsx
+++ src/components/organisms/Payroll/Payroll.tsx
@@ -14,12 +14,11 @@ const PayrollWrapper = styled.div`
 const TeamWrapper = styled.div`
   display: flex;
   justify-content: space-evenly;
-  align-items: center;
   text-align: center;
   border: 2px solid ${({ theme }) => theme.textColor};
   border-radius: 20px;
   margin: 1em;
-  padding: 1em;
+  padding: 1.5em;
   width: 24em;
   cursor: pointer;
   &:hover {
@@ -31,6 +30,16 @@ const TeamWrapper = styled.div`
   @media (max-width: 768px) {
     width: 90vw;
   }
+`;
+
+const Team = styled.div`
+  width: 60%;
+  padding: 1em;
+`;
+
+const Player = styled.div`
+  width: 40%;
+  padding: 1em;
 `;
 
 const Title = styled.p`
@@ -45,13 +54,14 @@ const Description = styled.p`
 `;
 
 const TeamLogo = styled.img`
-  width: 15em;
-  height: 12em;
+  width: 13em;
+  height: 13em;
+  padding: 1em;
 `;
 
 const PlayerImage = styled.img`
-  width: 8em;
-  height: 12em;
+  width: 9em;
+  height: 14em;
   border-radius: 20px;
 `;
 
@@ -69,18 +79,18 @@ const Payroll = () => {
     <PayrollWrapper>
       {teams?.map((team) => (
         <TeamWrapper key={team.rank} onClick={() => onClickTeam(team.key)}>
-          <div>
-            <Title>팀 정보</Title>
+          <Team>
+            <Title>랭킹 {team.rank}위</Title>
             <Description>팀 이름 : {team.team}</Description>
             <Description>연봉 총액 : {team.payroll}</Description>
             <TeamLogo src={team.teamLogoUrl} alt="Team Logo" />
-          </div>
-          <div>
+          </Team>
+          <Player>
             <Title>대표 선수</Title>
-            <Description>이름 : {team.bestPlayer}</Description>
-            <Description>포지션: {team.position}</Description>
+            <Description>{team.bestPlayer}</Description>
+            <Description>포지션 : {team.position}</Description>
             <PlayerImage src={team.playerImageUrl} alt="Player" />
-          </div>
+          </Player>
         </TeamWrapper>
       ))}
     </PayrollWrapper>
