2433ad3b5533a81dd35656deac28f392b436e7f5
이수동 <tnehd1998@gmail.com>
2022-03-30 13:34:34 +0900

    feat: Home 페이지 다른 페이지 이동 테스트 코드 요소 추가

--- cypress/integration/Home.spec.tsx
+++ cypress/integration/Home.spec.tsx
@@ -16,18 +16,28 @@ describe("Home Page", () => {
 
   it("moves to Los Angeles Dodgers page", () => {
     cy.get('[alt="Los Angeles Dodgers"]').click();
+    cy.contains("Los Angeles");
+    cy.contains("Dodgers");
   });
 
   it("moves to ranking page", () => {
     cy.contains("RANKING").click();
+    cy.contains("FA 랭킹 Top100 확인하기");
+    cy.contains("구단 연봉총액 순위 확인하기");
   });
 
   it("moves to record page", () => {
     cy.contains("RECORD").click();
+    cy.contains("2021 MLB POSTSEASON");
+    cy.contains("2021 TEAM STANDING");
   });
 
   it("moves to dream team page", () => {
     cy.contains("DREAM TEAM").click();
+    cy.contains("내야수");
+    cy.contains("외야수 / 지명타자");
+    cy.contains("선발 투수");
+    cy.contains("불펜 투수");
   });
 
   it("clicks dark mode", () => {
